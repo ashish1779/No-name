1c8b1a58203d7eaff3f44fbdbd0e1b39ad2ec4e0
Ashish Anand <32809264+ashish1779@users.noreply.github.com>
2018-10-11 20:36:48 +0530

    Create 1.1.ino

---
+++ 1.1.ino
@@ -0,0 +1,290 @@
+#include <PS3BT.h>
+#include <usbhub.h>
+
+#include <Servo.h>
+
+#ifdef dobogusinclude
+#include <spi4teensy3.h>
+#endif
+#include <SPI.h>      //Serial Peripheral Interface and it is a way to send data between microcontrollers and other small devices
+
+USB Usb;              //USBHub Hub1(&Usb); // Some dongles have a hub inside
+
+BTD Btd(&Usb);        // You have to create the Bluetooth Dongle instance like so
+                      /* You can create the instance of the class in two ways */
+PS3BT PS3(&Btd);    // This will just create the instance
+//PS3BT PS3(&Btd, 0x00, 0x15, 0x83, 0x3D, 0x0A, 0x57);    // This will also store the bluetooth address - 
+                                                        // this can be obtained from the dongle when running the sketch
+
+bool printTemperature, printAngle;       //for measuring temperature NO USE
+
+Servo myservo;  // create servo object to control a servo
+int pos = 0;    // variable to store the servo position
+
+int motorSpeedA = 0;
+int motorSpeedB = 0;
+int motorSpeedC = 0;
+int motorSpeedD = 0;
+
+int rx,ry,r2;
+
+#define enA 8
+#define in1 22
+#define in2 23
+#define enB 9
+#define in3 24
+#define in4 25
+#define enC 10
+#define in5 26
+#define in6 27
+#define enD 11
+#define in7 28
+#define in8 29
+
+
+void setup() {
+  Serial.begin(115200);
+
+    #if !defined(__MIPSEL__)
+          while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+    #endif
+  
+  if (Usb.Init() == -1) 
+    {Serial.print(F("\r\nOSC did not start"));
+      while (1); //halt
+    }
+    Serial.print(F("\r\nPS3 Bluetooth Library Started"));
+
+    pinMode(enA, OUTPUT);   //driver 1
+    pinMode(enB, OUTPUT);
+    pinMode(enC, OUTPUT);   //driver 2
+    pinMode(enD, OUTPUT);
+    pinMode(in1, OUTPUT);   //driver 1
+    pinMode(in2, OUTPUT);
+    pinMode(in3, OUTPUT);   
+    pinMode(in4, OUTPUT);
+    pinMode(in5, OUTPUT);   //driver 2
+    pinMode(in6, OUTPUT);
+    pinMode(in7, OUTPUT);   
+    pinMode(in8, OUTPUT);
+
+    myservo.attach(49);  // attaches the servo on pin 9 to the servo object
+}
+
+
+void loop() 
+{
+  Usb.Task();
+
+  if (PS3.PS3Connected || PS3.PS3NavigationConnected) 
+  { //Serial.println(PS3.getAnalogHat(RightHatY));Serial.println(PS3.getAnalogHat(RightHatX));
+    //RightHatY (Y-Axis of Right) used for forward and backward control
+      rx = PS3.getAnalogHat(RightHatX);
+      ry = PS3.getAnalogHat(RightHatY);
+      r2 = PS3.getAnalogButton(R2);
+    if (ry <= 115 && rx == 127)     //Forward
+    { Serial.println("Forward");
+      digitalWrite(in1,LOW);   //MOTOR A    // Also 1-LOW and 2-High for clockwise (Set accordingly)
+      digitalWrite(in2,HIGH);
+      
+      digitalWrite(in3,LOW);   //MOTOR B
+      digitalWrite(in4,HIGH);
+      
+      digitalWrite(in5,LOW);   //MOTOR C
+      digitalWrite(in6,HIGH);
+
+      digitalWrite(in7,LOW);   //MOTOR D
+      digitalWrite(in8,HIGH);
+
+      
+      motorSpeedA = map(ry, 115,0,0,255);
+      motorSpeedB = map(ry, 115,0,0,255);
+      motorSpeedC = map(ry, 115,0,0,255);
+      motorSpeedD = map(ry, 115,0,0,255);
+    }
+
+    else if (ry >= 140 && rx == 127)     //Backward
+    { Serial.println("Backward");
+      digitalWrite(in1,HIGH);   //MOTOR A
+      digitalWrite(in2,LOW);
+      
+      digitalWrite(in3,HIGH);   //MOTOR B
+      digitalWrite(in4,LOW);
+      
+      digitalWrite(in5,HIGH);   //MOTOR C
+      digitalWrite(in6,LOW);
+
+      digitalWrite(in7,HIGH);   //MOTOR D
+      digitalWrite(in8,LOW);
+
+      
+      motorSpeedA = map(ry, 140,255,0,255);
+      motorSpeedB = map(ry, 140,255,0,255);
+      motorSpeedC = map(ry, 140,255,0,255);
+      motorSpeedD = map(ry, 140,255,0,255);
+    }
+
+    // RightHatX (X-Axis of Right) is used for left and right control;
+    else if (rx >= 140 && ry == 127)     //Right
+    { Serial.println("Right");
+      digitalWrite(in1,LOW);   //MOTOR A   forward move
+      digitalWrite(in2,HIGH);
+      
+      digitalWrite(in3,HIGH);   //MOTOR B  backward move
+      digitalWrite(in4,LOW);
+      
+      digitalWrite(in5,HIGH);   //MOTOR C   backward move
+      digitalWrite(in6,LOW);
+
+      digitalWrite(in7,LOW);   //MOTOR D    forward move
+      digitalWrite(in8,HIGH);
+
+      
+      motorSpeedA = map(rx, 140,255,0,255);
+      motorSpeedB = map(rx, 140,255,0,255);
+      motorSpeedC = map(rx, 140,255,0,255);
+      motorSpeedD = map(rx, 140,255,0,255);
+    }
+
+    else if (rx < 115 && ry == 127)     //Left
+    { Serial.println("Left");
+      digitalWrite(in1,HIGH);   //MOTOR A    backward
+      digitalWrite(in2,LOW);
+      
+      digitalWrite(in3,LOW);   //MOTOR B    forward
+      digitalWrite(in4,HIGH);
+      
+      digitalWrite(in5,LOW);   //MOTOR C    forward
+      digitalWrite(in6,HIGH);
+
+      digitalWrite(in7,HIGH);   //MOTOR D    backward
+      digitalWrite(in8,LOW);
+
+      
+      motorSpeedA = map(rx, 115,0,0,255);
+      motorSpeedB = map(rx, 115,0,0,255);
+      motorSpeedC = map(rx, 115,0,0,255);
+      motorSpeedD = map(rx, 115,0,0,255);
+    }
+    
+    else if (rx < 127 && ry < 127)     //ForwardLeft
+    { Serial.println("ForwardLeft");
+      digitalWrite(in1,LOW);   //MOTOR A    stop
+      digitalWrite(in2,LOW);
+      
+      digitalWrite(in3,LOW);   //MOTOR B    forward
+      digitalWrite(in4,HIGH);
+      
+      digitalWrite(in5,LOW);   //MOTOR C    forward
+      digitalWrite(in6,HIGH);
+
+      digitalWrite(in7,LOW);   //MOTOR D    stop
+      digitalWrite(in8,LOW);
+
+      
+      motorSpeedA = 0;        //map(RightHatX, 127,0,0,0); //stop
+      motorSpeedB = 200;        //map(RightHatX, 127,0,0,255);
+      motorSpeedC = 200;        //map(RightHatX, 127,0,0,255);
+      motorSpeedD = 0;        //map(RightHatX, 127,0,0,0);
+    }
+    
+      else if (rx > 127 && ry < 127)     //Forwardright
+    { Serial.println("ForwardRight");
+      digitalWrite(in1,LOW);   //MOTOR A    forward
+      digitalWrite(in2,HIGH);
+      
+      digitalWrite(in3,LOW);   //MOTOR B    stop
+      digitalWrite(in4,LOW);
+      
+      digitalWrite(in5,LOW);   //MOTOR C    stop
+      digitalWrite(in6,LOW);
+
+      digitalWrite(in7,LOW);   //MOTOR D    forward
+      digitalWrite(in8,HIGH);
+
+      
+      motorSpeedA = 200;    //map(RightHatX, 127,0,0,255); //stop
+      motorSpeedB = 0;    //map(RightHatX, 127,0,0,0);
+      motorSpeedC = 0;    //map(RightHatX, 127,0,0,0);
+      motorSpeedD = 200;    //map(RightHatX, 127,0,0,255);
+    }
+
+
+     else if (r2 > 0)     //clockwiseturn
+    { Serial.println("Turn Clockwise");
+      digitalWrite(in1,LOW);   //MOTOR A    forward
+      digitalWrite(in2,HIGH);
+      
+      digitalWrite(in3,HIGH);   //MOTOR B    backward
+      digitalWrite(in4,LOW);
+      
+      digitalWrite(in5,LOW);   //MOTOR C    forward
+      digitalWrite(in6,HIGH);
+
+      digitalWrite(in7,HIGH);   //MOTOR D    backward
+      digitalWrite(in8,LOW);
+
+      
+      motorSpeedA = map(r2, 0,255,0,255); //clockwise turn
+      motorSpeedB = map(r2, 0,255,0,255);
+      motorSpeedC = map(r2, 0,255,0,255);
+      motorSpeedD = map(r2, 0,255,0,255);
+    }
+    
+    else
+    { 
+      motorSpeedA = 0;
+      motorSpeedB = 0;
+      motorSpeedC = 0;
+      motorSpeedD = 0;
+    }
+        //Use of Right Joystick is done in moving forward,backward,left and right
+
+    if (motorSpeedA < 50)       // Prevent buzzing of motor by stoping at slow speed 
+    { motorSpeedA = 0; }
+    if (motorSpeedB < 50) 
+    { motorSpeedB = 0; }
+    if (motorSpeedC < 50) 
+    { motorSpeedC = 0; }
+    if (motorSpeedD < 50) 
+    { motorSpeedD = 0; }
+    
+    analogWrite(enA, motorSpeedA); // Send PWM signal to motor A
+    analogWrite(enB, motorSpeedB); // Send PWM signal to motor B    
+    analogWrite(enC, motorSpeedC); // Send PWM signal to motor C
+    analogWrite(enD, motorSpeedD); // Send PWM signal to motor D    
+          Serial.println(motorSpeedA);
+          Serial.println(motorSpeedB);
+          Serial.println(motorSpeedC);
+          Serial.println(motorSpeedD);
+
+    if (PS3.getButtonClick(RIGHT))
+    { Serial.println("Servoclockwise");
+      servomotorclockwise();
+    }
+
+    if (PS3.getButtonClick(LEFT))
+    { Serial.println("Servoanticlockwise");
+      servomotoranticlockwise();
+    }
+
+}
+}
+
+void servomotorclockwise()
+{for (pos = 0; pos <= 180; pos += 1) 
+  { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
+
+void servomotoranticlockwise()
+{  
+  for (pos = 180; pos >= 0; pos -= 1) 
+  { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position}
+  }
+}
